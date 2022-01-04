a9a1654d18be1be2b31e9b3854413483735ce3aa
G_R <45249429+Ghost-Rider10@users.noreply.github.com>
2022-01-04 18:15:38 +0530

    Add files via upload

---
+++ Debloat.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+TITLE DEBLOATER
+ECHO Debloater for XIOMI MIUI 
+ECHO ====================================
+ECHO Detecting attached devices
+ECHO ====================================
+adb devices
+ECHO ====================================
+ECHO Starting ...... (Ignore Errors as they are not installed already.)
+FOR /F "tokens=* delims=" %%x in (Debloat.txt) DO (
+	ECHO REMOVING %%x
+	adb shell pm uninstall -k --user 0 %%x
+)
+ECHO Done !!
+PAUSE
