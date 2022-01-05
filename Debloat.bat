0f7e37454451d12ac4f20ed3049e5550a571d3f6
G_R <45249429+Ghost-Rider10@users.noreply.github.com>
2022-01-05 15:55:47 +0530

    New Feature Added
    Now users can add the path to ADB and also a name for the Debloat File.

--- Debloat.bat
+++ Debloat.bat
@@ -1,15 +1,16 @@
 @ECHO OFF
 TITLE DEBLOATER
-ECHO Debloater for XIOMI MIUI 
+
+ECHO Debloater for ANDROID
 ECHO ====================================
 ECHO Detecting attached devices
 ECHO ====================================
 adb devices
 ECHO ====================================
 ECHO Starting ...... (Ignore Errors as they are not installed already.)
-FOR /F "tokens=* delims=" %%x in (MIUI_Debloat.txt) DO (
+FOR /F "tokens=* delims=" %%x in (%2) DO (
 	ECHO REMOVING %%x
-	adb shell pm uninstall -k --user 0 %%x
+	%1\adb shell pm uninstall -k --user 0 %%x
 )
 ECHO Done !!
 PAUSE
